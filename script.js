92ef460ae17f36736165382bf66064e9533843cc
Praise <praisemichael93@gmail.com>
2023-11-08 09:27:49 +0100

    complete

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // the reason for the let here is to declare it so we can change the value in the getQuotes function and we cannot use const for it because if we use const we cannot change the value.
 let apiQuotes = [];
 
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // show new quote
 function newQuote(){
+    loading();
     // math floor is to know the largest number
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check if author field is blank
@@ -21,11 +35,14 @@ function newQuote(){
     if (quote.text.length > 50){
         quoteText.classList.add('long-quote');
     }
+    // set Quote, Hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
@@ -37,10 +54,6 @@ async function getQuotes() {
     }
 }
 
-// On Load
-getQuotes();
-
-
 //  Tweet quote
 function tweetQuote() {
     const twitterUrl = `https://twitter.com/compose/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
@@ -51,4 +64,5 @@ function tweetQuote() {
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-// newQuote();
+// On Load
+getQuotes();
